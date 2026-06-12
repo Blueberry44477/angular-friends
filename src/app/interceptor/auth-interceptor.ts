544ada29372a3770540a73ed9a6e379f6984ae36
Blueberry44477 <vas172674@gmail.com>
2026-06-12 17:08:33 +0300

    feat: add access token refreshment on unauthorized response

---
+++ src/app/interceptor/auth-interceptor.ts
@@ -0,0 +1,49 @@
+import { HttpInterceptorFn } from "@angular/common/http";
+import { catchError, switchMap, throwError } from "rxjs";
+import { AuthService } from "../service/auth-service";
+import { inject } from "@angular/core";
+import { AccessTokenDTO } from "../dto/access-token-dto";
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+    const authService = inject(AuthService);
+    const accessTokenData: AccessTokenDTO | null = authService.getAccessTokenData();
+
+    let modifiedReq = req;
+    if (accessTokenData) {
+        modifiedReq = req.clone({
+            setHeaders: {
+                Authorization: `${accessTokenData.tokenType} ${accessTokenData.accessToken}`
+            }
+        });
+    } else {
+        modifiedReq = req.clone({
+            withCredentials: true
+        });
+    }
+
+    return next(modifiedReq).pipe(
+        catchError((error) => {
+            if (error.status === 401) {
+                return authService.refreshTokens().pipe(
+                    switchMap((accessTokenDto: AccessTokenDTO) => {
+                        authService.setAccessTokenData(accessTokenDto);
+
+                        const clonedWithNewToken = req.clone({
+                            setHeaders: {
+                                Authorization: `${accessTokenDto.tokenType} ${accessTokenDto.accessToken}`
+                            }
+                        });
+
+                        return next(clonedWithNewToken);
+                    }),
+                    catchError((refreshError) => {
+                        authService.logout();
+                        return throwError(() => refreshError);
+                    })
+                );
+            }
+
+            return throwError(() => error);
+        })
+    );
+};
