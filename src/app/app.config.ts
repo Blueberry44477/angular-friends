39eb18184e446f044f10bd94d862ae6b4782fa1e
Blueberry44477 <vas172674@gmail.com>
2026-06-12 20:21:26 +0300

    feat: implement access token refreshment on page reload
    Persist and restore the session on reload so that authenticated users are not inadvertently logged out when refreshing the page.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptor/auth-interceptor';
+import { AuthService } from './service/auth-service';
+import { catchError, of, take } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAppInitializer(() => {
+      const authService = inject(AuthService);
+
+      return authService.refreshTokens().pipe(
+        take(1),
+        catchError(() => of(null))
+      );
+    })
   ]
 };
